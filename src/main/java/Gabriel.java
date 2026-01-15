3f1679fe3d1cdef7a8630aedb17de042d58067f7
Gabriel Phua <e1121596@u.nus.edu>
2026-01-15 21:39:02 +0800

    Level 0, renamed file and added greetings

---
+++ src/main/java/Gabriel.java
@@ -0,0 +1,11 @@
+public class Gabriel {
+    public static void main(String[] args) {
+        String Greetings = "--------------------\n"
+                + "Hello! I'm Gabriel \n"
+                + "What can I do for you? \n"
+                + "--------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "--------------------";
+        System.out.println(Greetings);
+    }
+}
