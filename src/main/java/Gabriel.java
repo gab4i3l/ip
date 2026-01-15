554df3a760ba619c90b22ef491eefc5e0b1e07a3
Gabriel Phua <e1121596@u.nus.edu>
2026-01-15 21:57:59 +0800

    Level 1, Added Echo

--- src/main/java/Gabriel.java
+++ src/main/java/Gabriel.java
@@ -1,11 +1,25 @@
+import java.util.Scanner; //For user input
 public class Gabriel {
     public static void main(String[] args) {
-        String Greetings = "--------------------\n"
+        String Indentations = "--------------------";
+        String Greetings = Indentations + "\n"
                 + "Hello! I'm Gabriel \n"
                 + "What can I do for you? \n"
-                + "--------------------\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "--------------------";
+                + Indentations + "\n";
+
         System.out.println(Greetings);
+        System.out.println("Enter something and I will echo it back to you: \n");
+        Scanner myScanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(Indentations + "\n" + input + "\n" + Indentations);
+        }
+        String exitMessage = Indentations + "\n" + "Bye. Hope to see you again soon!\n"
+                + Indentations;
+        System.out.println(exitMessage);
     }
 }
