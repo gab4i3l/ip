4068b95957bdee8dff879876474575a310155d51
Gabriel Phua <e1121596@u.nus.edu>
2026-01-16 11:17:57 +0800

    Level 3, Added Mark as Done and Unmark

--- src/main/java/Gabriel.java
+++ src/main/java/Gabriel.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; //For user input
 import java.util.ArrayList;
 public class Gabriel {
-    static ArrayList<String> myItems = new ArrayList<>();
+    static ArrayList<Task> myTask = new ArrayList<>();
     public static void main(String[] args) {
         String Indentations = "--------------------";
         String Greetings = Indentations + "\n"
@@ -15,26 +15,44 @@ public class Gabriel {
         String input = "";
         while (true) {
             input = myScanner.nextLine();
-            if (input.equals("bye")) {
-                break;
+            String[] parts = input.split(" ");
+            String command = parts[0];
+
+            switch(command){
+                case "bye":
+                    String exitMessage = Indentations + "\n" + "Bye. Hope to see you again soon!\n"
+                            + Indentations;
+                    System.out.println(exitMessage);
+                    return;
+                case "list":
+                    System.out.println(Indentations);
+                    listTaskItems(myTask);
+                    System.out.println(Indentations);
+                    break;
+                case "mark":
+                    System.out.println(Indentations);
+                    int markIndex = Integer.parseInt(parts[1]) - 1;
+                    myTask.get(markIndex).setDone(true);
+                    System.out.println(Indentations);
+                    break;
+                case "unmark":
+                    System.out.println(Indentations);
+                    int unmarkIndex = Integer.parseInt(parts[1]) - 1;
+                    myTask.get(unmarkIndex).setDone(false);
+                    System.out.println(Indentations);
+                    break;
+                default:
+                    myTask.add(new Task(input));
+                    System.out.println(Indentations + "\n" + "added: " + input + "\n" + Indentations);
+                    break;
             }
-            if (input.equals("list")){
-                System.out.println(Indentations);
-                listItems(myItems);
-                System.out.println(Indentations);
-                continue;
-            }
-            myItems.add(input);
-            System.out.println(Indentations + "\n" + "added: " + input + "\n" + Indentations);
         }
-        String exitMessage = Indentations + "\n" + "Bye. Hope to see you again soon!\n"
-                + Indentations;
-        System.out.println(exitMessage);
     }
-    public static void listItems(ArrayList<String>myItems){
+    public static void listTaskItems(ArrayList<Task>myTask){
+        System.out.println("Here are the tasks in your list:");
         int count = 1;
-        for (String item: myItems){
-            System.out.println(count + ". " + item);
+        for (Task task: myTask){
+            System.out.println(count + ".[" + task.getStatusIcon()+ "] " + task.getDescription());
             count++;
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X": " ");
+    }
+
+    public void setDone(boolean isDone) {
+        if (!isDone) {
+            this.isDone = false;
+            System.out.println("OK, I've marked this task as not done yet: \n" +
+                    "[ ] " + getDescription());
+
+        }
+        else {
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done: \n" +
+                    "[X] " + getDescription());
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
