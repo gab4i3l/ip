2ee71dccd9e0af4fe741f897fdd50cb685e08e27
Gabriel Phua <e1121596@u.nus.edu>
2026-01-16 20:21:32 +0800

    Level 5 and 6, added error and delete

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,21 +1,22 @@
 @ECHO OFF
 
-REM create bin directory if it doesn't exist
+REM Create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM Delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
+REM Compile all Java files into the bin folder
+REM Note: -cp points to the directory containing source files
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+REM Run the program
+REM Note: -classpath points to the 'bin' folder where compiled .class files are
+java -classpath ..\bin Gabriel < input.txt > ACTUAL.TXT
 
-REM compare the output to the expected output
+REM Compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
