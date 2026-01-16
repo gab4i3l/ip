f6197c571f0d9f174f4c2bf8a57e08a0ab1ed4dc
Gabriel Phua <e1121596@u.nus.edu>
2026-01-16 20:30:04 +0800

    Tried adding some tests case, need to change the indentation

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,22 +1,21 @@
 @ECHO OFF
 
-REM Create bin directory if it doesn't exist
+REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM Delete output from previous run
+REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM Compile all Java files into the bin folder
-REM Note: -cp points to the directory containing source files
+REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
+REM no error here, errorlevel == 0
 
-REM Run the program
-REM Note: -classpath points to the 'bin' folder where compiled .class files are
+REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Gabriel < input.txt > ACTUAL.TXT
 
-REM Compare the output to the expected output
+REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
